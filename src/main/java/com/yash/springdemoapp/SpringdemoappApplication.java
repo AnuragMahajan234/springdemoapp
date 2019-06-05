f12434671dd42783b62c5a66dbb48926f86a03ae
AnuragMahajan234 <anuragmahajan1711@gmail.com>
2019-06-05 11:06:39 +0530

    Update SpringdemoappApplication.java

--- src/main/java/com/yash/springdemoapp/SpringdemoappApplication.java
+++ src/main/java/com/yash/springdemoapp/SpringdemoappApplication.java
@@ -2,14 +2,17 @@ package com.yash.springdemoapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class SpringdemoappApplication extends SpringBootServletInitializer{
-
+	
 	 @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-         return application.sources(SpringdemoappApplication.class);
-    }
+	    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	         return application.sources(SpringdemoappApplication.class);
+	    }
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpringdemoappApplication.class, args);
 	}
